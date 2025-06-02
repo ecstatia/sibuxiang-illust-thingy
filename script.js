0818ed4c08d2407c7dfd1c55ca70e0a72dd51c7b
ecstatia <meemawjune3@gmail.com>
2025-06-02 23:27:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const script = document.createElement("script");
+script.type = "module"; // Mark it as a module
+script.src = "https://cdn.jsdelivr.net/gh/ecstatia/repository/script.js";
+document.body.appendChild(script);
+
+
 let app = new PIXI.Application({ width: window.innerWidth, height: window.innerHeight });
 document.body.appendChild(app.view);
 
